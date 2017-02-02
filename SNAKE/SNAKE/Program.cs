cac17e3235b2ac415e29c463f0a1de87387c3fea
Чуприков Андрей Вячеславович <chuprikov@Nota.Local>
2017-02-02 11:33:41 +0300

    домашнее задание по списку

--- SNAKE/SNAKE/Program.cs
+++ SNAKE/SNAKE/Program.cs
@@ -38,6 +38,25 @@ namespace SNAKE
             pList.Add(p1);
             pList.Add(p2);
 
+            Point p1_my = new Point(10, 10, '!');
+            p1_my.Draw(p1_my.x, p1_my.y, p1_my.sym);
+
+            Point p2_my = new Point(10, 12, '@');
+            p2_my.Draw(p2_my.x, p2_my.y, p2_my.sym);
+
+            Point p3_my = new Point(10, 14, '#');
+            p3_my.Draw(p3_my.x, p3_my.y, p3_my.sym);
+
+            Point p4_my = new Point(10, 16, '$');
+            p4_my.Draw(p4_my.x, p4_my.y, p4_my.sym);
+
+            List<Point> pList_my = new List<Point>();
+
+            pList_my.Add(p1_my);
+            pList_my.Add(p2_my);
+            pList_my.Add(p3_my);
+            pList_my.Add(p4_my);
+
             Console.ReadLine();
         }
 
