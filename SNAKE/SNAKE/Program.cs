3ff56f3213d02053045ec5afdb4d12c3f13dbfcd
Чуприков Андрей Вячеславович <chuprikov@Nota.Local>
2017-02-02 11:05:29 +0300

    осваиваем список

--- SNAKE/SNAKE/Program.cs
+++ SNAKE/SNAKE/Program.cs
@@ -10,12 +10,33 @@ namespace SNAKE
     {
         static void Main(string[] args)
         {
-
             Point p1 = new Point(1, 3, '*');
             p1.Draw(p1.x, p1.y, p1.sym);
 
             Point p2 = new Point(4, 5, '#');
             p2.Draw(p2.x, p2.y, p2.sym);
+
+            List<int> numList = new List<int>();
+
+            numList.Add(0);
+            numList.Add(1);
+            numList.Add(2);
+
+            int x = numList[0];
+            int y = numList[1];
+            int z = numList[2];
+
+            foreach (int i in numList)
+            {
+                Console.WriteLine(i);
+            }
+
+            numList.RemoveAt(0);
+
+            List<Point> pList = new List<Point>();
+
+            pList.Add(p1);
+            pList.Add(p2);
 
             Console.ReadLine();
         }
